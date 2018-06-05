93c110e3a57bcfc02eb93397aec531817712d2a9
Thanut Sajjakulnukit <thanut.sjknk@gmail.com>
2018-06-05 13:35:32 +0700

    Create view, model and viewModel

--- DemoMVVM/ViewController.swift
+++ DemoMVVM/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Image: UIImageView!
+    @IBOutlet weak var NameLabel: UILabel!
+    
+    var viewModel: PetViewModel? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        viewModel = PetViewModel()
+        updateContent(pet: (viewModel?.pet)!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func updateContent(pet: Pet) {
+        Image.image = pet.Image
+        NameLabel.text = pet.Name
+    }
 
-
+    @IBAction func NextBtn(_ sender: Any) {
+        viewModel?.next()
+        updateContent(pet: (viewModel?.pet)!)
+    }
+    
 }
 
